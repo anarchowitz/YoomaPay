7e7013b40bf0c3c5d0f43ed6fbc8ad2403b1ace8
anarchowitz <anarchowitz>
2025-03-27 22:08:04 +0300

    added "promocode" column in profiles added promocode table

--- database.py
+++ database.py
@@ -9,9 +9,22 @@ cursor.execute('''
         telegram_id INTEGER,
         profile_url TEXT,
         purchases INTEGER DEFAULT 0,
-        join_date DATE
+        join_date DATE,
+        promocode TEXT
     )
 ''')
 conn.commit()
 
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS promocodes (
+        id INTEGER PRIMARY KEY,
+        code TEXT,
+        discount INTEGER,
+        expiration_date DATE,
+        used INTEGER DEFAULT 0,
+        max_uses INTEGER,
+        telegram_id_used_promo TEXT
+    )
+''')
+conn.commit()
 
